bef57653dc7a2db53729eb9d676b8ea6dd83b37b
Miriam Suzanne <miriam@oddbird.net>
2022-12-20 16:48:09 -0700

    Flesh out the site a bit, and link @mods

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,25 @@
+const yaml = require('js-yaml');
+
+const time = require('./plugins/time');
+
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addWatchTarget("./src/sass/");
+  // config plugins
+  eleventyConfig.addPlugin(time);
+
+  // pass-through
+  eleventyConfig.addPassthroughCopy({'./src/_images': 'images'});
+
+  // config
+  eleventyConfig.addWatchTarget('./src/sass/');
+  eleventyConfig.addDataExtension('yaml', yaml.load);
 
   return {
+    markdownTemplateEngine: 'njk',
     dir: {
-      input: "src",
-      output: "public",
+      input: 'src',
+      output: 'public',
+      includes: '_includes',
+      layouts: '_layouts',
     },
   };
 };
